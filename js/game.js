c677d04c7a9f5c11ff515e8bbc85cbed8ff95102
Mko Galoyan <mko@gmail.com>
2016-11-26 14:37:17 +1100

    fixed screen scaling, added ball

--- js/game.js
+++ js/game.js
@@ -1,17 +1,28 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, null, {
+var game = new Phaser.Game(800, 450, Phaser.AUTO, null, {
       preload: preload, create: create, update: update
     });
 
 
 function preload() {
-
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
 	game.load.image('background', 'assets/background.png');
+	game.load.image('ball','assets/ball.png')
 }
 
+
+
 var platforms;
+var ball;
 
 function create() {
-	ground = game.add.sprite(0,0, 'background');
+
+	background = game.add.sprite(0,0, 'background');
+	ball = game.add.sprite(50, 50, 'ball');
+	ball.scale.setTo(0.5, 0.5);
+	
+
 
 }
 
