131628c9a56db1a9f5f846742cf844ccd506aec5
Sam Gardner <Sam789444@gmail.com>
2016-11-26 13:52:17 +1100

    added background png and internal code

--- js/game.js
+++ js/game.js
@@ -1,9 +1,23 @@
-var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {
+var game = new Phaser.Game(800, 600, Phaser.AUTO, null, {
       preload: preload, create: create, update: update
     });
 
 
-function preload() {}
-function create() {}
-function update() {}
+function preload() 
+{
+	game.load.image('background', 'assets/background.png');
+}
+
+var platforms;
+
+function create() 
+{
+	ground = game.add.sprite(0,0, 'background');
+
+}
+function update() 
+{
+
+
+}
 
