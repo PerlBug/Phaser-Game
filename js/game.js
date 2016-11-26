1c8e499348cfe1dd268b3ba8bf135eea21c5a9e4
Sam Gardner <Sam789444@gmail.com>
2016-11-26 14:55:50 +1100

    added ground.png and floor in game

--- js/game.js
+++ js/game.js
@@ -6,17 +6,24 @@ var game = new Phaser.Game(800, 450, Phaser.AUTO, null, {
 function preload() 
 {
 	game.load.image('background', 'assets/background.png');
+	game.load.image('ground', 'assets/ground.png');
 }
 
 var platforms;
 
+
 function create() 
 {
-	platforms = game.add.group();
-
+	
 	background = game.add.sprite(0,0, 'background');
 
+	platforms = game.add.group();
+	var ground = platforms.create(0, game.world.height - 50, 'ground');
+	ground.scale.setTo(8,1);
+	
+
 }
+
 function update() 
 {
 
